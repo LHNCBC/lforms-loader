305c050d1f2e9990eaef08be8df59e9a88e91607
Paul Lynch <plynch@mail.nih.gov>
2023-07-28 08:58:38 -0400

    Added tests for the optional loadLForms() parameters

--- test/cypress/e2e/exports.cy.js
+++ test/cypress/e2e/exports.cy.js
@@ -3,13 +3,46 @@ describe('loadLForms', ()=>{
   it('should define LForms', ()=>{
     cy.visit('test/pages/testPage.html');
     cy.window().then(win=>{
-      // I wanted to test whether LForms was undefined, but Cypress tries to
-      // make a string of it, which results in an exception be throws.  Checking
-      // just for LForms.lformsVersion should suffice.
-      expect(win.LForms.lformsVersion).to.be.not.undefined;
+      expect(win.LForms).to.be.undefined;
+      win.loadLForms('33.4.2').then(()=>LForms.Util.addFormToPage(form, 'formDiv')).then(()=>{
+        // I wanted to test whether LForms was undefined, but Cypress tries to
+        // make a string of it, which results in an exception be throws.  Checking
+        // just for LForms.lformsVersion should suffice.
+        expect(win.LForms.lformsVersion).to.be.not.undefined;
+      });
+    });
+  });
+
+  it('should support a callback parameter', ()=>{
+    // The callback is triggered when the CSS file is loaded, but I think we can
+    // just test that the callback is called.
+    cy.visit('test/pages/testPage.html');
+    const callback = cy.stub();
+    cy.window().then(win=>{
+      win.loadLForms('33.4.2', callback).then(()=>LForms.Util.addFormToPage(form, 'formDiv')).then(()=>{
+        expect(callback).to.be.called;
+      });
+    });
+  });
+
+  it('should support a URL parameter for fetching LForms', ()=>{
+    // This parameter allows LForms to be fetched from an alternate location
+    // (e.g., a local installation).  Here we are using the fact that currently
+    // https://lforms-service.nlm.nih.gov/lforms-versions does not redirect to
+    // https://clinicaltables.nlm.nih.gov/lforms-versions, though perhaps it
+    // should since it is the same host.
+    cy.visit('test/pages/testPage.html');
+    cy.window().then(win=>{
+      win.loadLForms('33.4.2', null,
+        'https://lforms-service.nlm.nih.gov/lforms-versions').then(()=>
+          LForms.Util.addFormToPage(form, 'formDiv')).then(()=>{
+
+        expect(win.LForms.lformsVersion).to.be.not.undefined;
+      });
     });
   });
 });
+
 
 describe('getSupportedLFormsVersions', ()=>{
   it('should be return the LForms versions in order of most recent to least', ()=>{
